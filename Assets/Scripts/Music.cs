54e78a591022f43261f19c4f67ee95986f4361db
nisinghal <nishthasinghal.in@gmail.com>
2020-11-23 00:22:10 +0530

    Final Prototype v1

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource musicSource;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void playMusic()
+    {
+        musicSource.Play();
+    }
+    public void pauseMusic()
+    {
+        musicSource.Pause();
+    }
+    public void stopMusic()
+    {
+        musicSource.Stop();
+    }
+}
